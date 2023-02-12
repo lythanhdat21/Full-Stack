6abfaae52d40bc639bcb7bfa78d69ed10f134c8e
Tony Lee <lythanhdat21@gmail.com>
2023-02-12 22:45:23 +0700

    Lesson 20 Set State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,8 +6,19 @@ class MyComponent extends React.Component{
         age: 32
     }
 
-    handleClick(event){
-        console.log("My name is", this.state.name)
+    handleClick = (event)=>{
+        // console.log("My name is", this.state.name)
+        // console.log("radom ", Math.floor((Math.random()*100) + 1))
+        
+        // merge State => react class
+        this.setState({
+            name: "Lee",
+            age: Math.floor((Math.random()*100) + 1)
+        })
+
+        // this.setState({
+        //     age: Math.floor((Math.random()*100) + 1)
+        // })
     }
 
     handleOnMouseOver(event){
@@ -18,8 +29,10 @@ class MyComponent extends React.Component{
     render(){
         return(
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
-                <button onClick={this.handleClick}>Click me</button>
+                {/* My name is {this.state.name} and I'm from {this.state.address} */}
+                My name is {this.state.name} and I'm from {this.state.age}
+
+                <button onClick = {(event) =>{this.handleClick(event)}}>Click me</button>
                 <button onMouseOver ={this.handleOnMouseOver}>Mouse Over</button>
             </div>
         )
