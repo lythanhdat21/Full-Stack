29fb8b8de148d52b9ad97d593665d007e2316471
Tony Lee <lythanhdat21@gmail.com>
2023-02-13 22:09:08 +0700

    Lesson 21 Form with React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,33 +7,39 @@ class MyComponent extends React.Component{
     }
 
     handleClick = (event)=>{
-        // console.log("My name is", this.state.name)
-        // console.log("radom ", Math.floor((Math.random()*100) + 1))
         
         // merge State => react class
         this.setState({
             name: "Lee",
             age: Math.floor((Math.random()*100) + 1)
         })
-
-        // this.setState({
-        //     age: Math.floor((Math.random()*100) + 1)
-        // })
     }
 
     handleOnMouseOver(event){
         console.log(event.pageX)
         }
     
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
     // JSX
     render(){
         return(
             <div>
-                {/* My name is {this.state.name} and I'm from {this.state.address} */}
-                My name is {this.state.name} and I'm from {this.state.age}
+                My name is {this.state.name} and I'm {this.state.age} years old.
+                <form onSubmit ={(event) => this.handleOnSubmit(event)}>
+                    <input type = "text" onChange={(event) => this.handleOnChangeInput(event)}/>
+                    <button>Submit</button>
+                </form>
 
-                <button onClick = {(event) =>{this.handleClick(event)}}>Click me</button>
-                <button onMouseOver ={this.handleOnMouseOver}>Mouse Over</button>
             </div>
         )
     }
