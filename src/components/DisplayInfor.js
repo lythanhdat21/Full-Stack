68232639528492003a23bbba51eff1a5d759da93
Tony Lee <lythanhdat21@gmail.com>
2023-03-05 13:42:57 +0700

    Lesson 33 React LifeCycle

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,20 +3,44 @@ import './DisplayInfor.scss'
 import logo from './../logo.svg'
 
 class DisplayInfor extends React.Component {
+    constructor(props){
+        console.log('>>> call constructor: 1')
+        super(props)
+        this.state = {
+            isShowListUser: true
+        }
+    }
 
-    state = {
-        isShowListUser: true
+    // babel compiler
+    // state = {
+    //     isShowListUser: true
+    // }
+
+    componentDidMount(){
+        console.log('>>> call me component did mount')
+        setTimeout (() => {
+            document.title = "Tony & Hoi Dan IT"
+        }, 3000)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('>>> call me component did update', this.props, prevProps)
+        
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if(this.props.listUsers.length === 5){
+                alert('You got 5 users')
+            }
+        }
     }
 
     handleShowHide = () =>{
-
         this.setState({
             isShowListUser: !this.state.isShowListUser
-
         })
     }
 
     render(){
+        console.log('>>> call me render')
 
         //Destructuring Array/object
         const {listUsers} = this.props
