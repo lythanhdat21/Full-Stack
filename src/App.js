93b8c7fb97cc87af8e03bf150ea8f6904c842515
Tony Lee <lythanhdat21@gmail.com>
2023-03-12 06:46:47 +0700

    Lessson 46 Setup Bootrap 5 & React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,52 +1,32 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react'
+// import logo from './logo.svg';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+// import MyComponent from './components/MyComponent';
+// import React from 'react'
 
-class App extends React.Component{
-  render() {
-    return (
-      <div className = "app-container">
-        Hello World với Tony &amp; Hoi dân IT
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+const App = () => {
+
+  return (
+    <div className="app-container">
+        HELLO WORLD
+        {/* <button>test</button> */}
+    </div>
+  )
 }
-
-// const App = () => {
-  // const count = useSelector(state => state.counter.count);
-  // const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       Hello World với Tony &amp; Hoi dân IT
-//       <MyComponent></MyComponent>
-//     </div>
-//   );
-// }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           HELLO WORLD HOI DAN IT
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-
-//   );
-// }
-
 export default App;
+
+
+// class App extends React.Component{
+//   render() {
+//     return (
+//       <div className = "app-container">
+//         Hello World với Tony &amp; Hoi dân IT
+//         <MyComponent></MyComponent>
+//       </div>
+//     )
+//   }
+// }
+
+
 
