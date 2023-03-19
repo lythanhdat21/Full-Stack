fc048639e8def9349c4428a859264cb4060f2fc1
Tony Lee <lythanhdat21@gmail.com>
2023-03-19 13:05:25 +0700

    Lesson 50 Active Link - NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
@@ -13,9 +14,13 @@ const Header = () => {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
 
-                    <Link to="/" className='nav-link'>Home</Link>
+                    {/* <Link to="/" className='nav-link'>Home</Link>
                     <Link to="/users" className='nav-link'>Users</Link>
-                    <Link to="/admins" className='nav-link'>Admin</Link>
+                    <Link to="/admins" className='nav-link'>Admin</Link> */}
+
+                    <NavLink to="/" className='nav-link'>Home</NavLink>
+                    <NavLink to="/users" className='nav-link'>Users</NavLink>
+                    <NavLink to="/admins" className='nav-link'>Admin</NavLink>
                     
                 </Nav>
                 <Nav>
