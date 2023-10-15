3e31a1b3a0fab47d84f83d6db122f55ac6ae491a
Tony Lee <lythanhdat21@gmail.com>
2023-10-15 22:43:25 +0700

    Lesson 80 Redux persist - Xu ly data khi refresh

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,17 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducer/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
-
-export default store;
-
 // Quy định các thông tin sẽ lưu (thông qua reducer) và cấu hình cho redux (middleware như redux devtool, redux thunk…)
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+let persistor = persistStore(store)
+export { store, persistor }
