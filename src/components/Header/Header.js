070141fc2d2c9a53804e15ebfb3fed814dcddb4f
Tony Lee <lythanhdat21@gmail.com>
2024-08-22 22:57:25 +0700

    Lesson 113 Bai tap thiet ke profile, update password

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,6 +9,8 @@ import { toast } from 'react-toastify';
 import { doLogout } from '../../redux/action/userAction';
 import Language from './Language';
 import { useTranslation} from "react-i18next";
+import Profile from './Profile';
+import { useState } from 'react';
 
 const Header = () => {
     const account = useSelector(state => state.user.account)
@@ -16,6 +18,7 @@ const Header = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate();
     const {t} = useTranslation()
+    const [isshowModalProfile, setIsShowModalProfile] = useState(false)
 
     const handleLogin = () => {
         navigate('/login')
@@ -39,54 +42,57 @@ const Header = () => {
     }
 
     return (
-    <Navbar bg="light" expand="lg">
-        <Container>
-            <NavLink to="/" className='navbar-brand'>Hỏi Dân IT</NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <NavLink to="/" className='nav-link'>
-                        {/* Home */}
-                        {t('header.home')}
-                    </NavLink>
-                    <NavLink to="/users" className='nav-link'>
-                        {/* Users */}
-                        {t('header.user')}
-                    </NavLink>
-                    <NavLink to="/admins" className='nav-link'>
-                        {/* Admin */}
-                        {t('header.admin')}
-                    </NavLink>                   
-                </Nav>
-                <Nav>
-                    {isAuthenticated === false ? // Trường hợp chưa đăng nhập:
-                        <>
-                            <button className = 'btn-login' onClick = {() => handleLogin()}>Log in</button>
-                            <div className='aaa'>
-                                <button className = 'btn-signup' onClick = {() => handleRegister()}>
-                                    <span className='bbb'>Sign up</span>
-                                </button>
-                            </div>
-                        </>
-                        : // Trường hợp đã đăng nhập thành công
-                        // <NavDropdown title = "Settings" id = "basic-nav-dropdown">
-                        <NavDropdown title = {t('header.setting')} id = "basic-nav-dropdown">
-                            <NavDropdown.Item>
-                                {/* Profile */}
-                                {t('header.profile')}
-                            </NavDropdown.Item>
-                            <NavDropdown.Item onClick = {() => handleLogout()}>
-                                {/* Log out */}
-                                {t('header.logout')}
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    }
-                    <Language/>
-                    
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
+    <>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <NavLink to="/" className='navbar-brand'>Hỏi Dân IT</NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink to="/" className='nav-link'>
+                            {/* Home */}
+                            {t('header.home')}
+                        </NavLink>
+                        <NavLink to="/users" className='nav-link'>
+                            {/* Users */}
+                            {t('header.user')}
+                        </NavLink>
+                        <NavLink to="/admins" className='nav-link'>
+                            {/* Admin */}
+                            {t('header.admin')}
+                        </NavLink>                   
+                    </Nav>
+                    <Nav>
+                        {isAuthenticated === false ? // Trường hợp chưa đăng nhập:
+                            <>
+                                <button className = 'btn-login' onClick = {() => handleLogin()}>Log in</button>
+                                <div className='aaa'>
+                                    <button className = 'btn-signup' onClick = {() => handleRegister()}>
+                                        <span className='bbb'>Sign up</span>
+                                    </button>
+                                </div>
+                            </>
+                            : // Trường hợp đã đăng nhập thành công
+                            <NavDropdown title = {t('header.setting')} id = "basic-nav-dropdown">
+                                <NavDropdown.Item onClick = {() => setIsShowModalProfile(true)}>
+                                    {t('header.profile')}
+                                </NavDropdown.Item >
+                                <NavDropdown.Item onClick = {() => handleLogout()}>
+                                    {t('header.logout')}
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        }
+                        <Language/>
+                        
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        <Profile
+            show = {isshowModalProfile}
+            setShow = {setIsShowModalProfile}
+        />
+    </>
     );
 }
 
---
+++ src/components/Header/Profile.js
@@ -0,0 +1,48 @@
+// import { Modal } from 'bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { useTranslation} from "react-i18next";
+
+
+const Profile = (props) => {
+    const {t} = useTranslation()
+    const {show, setShow} = props
+    const handleClose = () => setShow(false);
+
+    return (
+        <>
+            <Modal
+                show = {show}
+                onHide = {handleClose}
+                size = "xl"
+                backdrop = "static"
+                className = "modal-profile"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>{t('profile.title')}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Tabs
+                        defaultActiveKey="profile"
+                        id="uncontrolled-tab-example"
+                        className="mb-3"
+                    >
+                        <Tab eventKey="home" title="Main Infor">
+                            Your Information
+                        </Tab>
+                        <Tab eventKey="profile" title="Password">
+                            Change Password
+                        </Tab>
+                        <Tab eventKey="history" title="History">
+                            Doing Quiz
+                        </Tab>
+                    </Tabs>
+                </Modal.Body>
+            </Modal>
+        </>
+  );
+}
+
+export default Profile;
+
